421e983ea069cb2d3d943d61eb9bdb22c4a15943
Mai Frey <hayamai@uw.edu>
2020-05-26 20:55:35 -0700

    fixed total participant call

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,9 +1,14 @@
+library("dplyr")
 get_summary_info <- function(dataset) {
   ret <- list()
   ret$length <- length(dataset)
   ret$type_of_questions <- unique(dataset$Question)
   ret$type_of_answer <- unique(dataset$Answer)
-  ret$total_participant <-sum(dataset$Count)
+  ret$total_participant <- dataset %>%
+    filter(Segment.Type == "Gender") %>%
+    select(Count) %>%
+    sum()
   ret$type_of_participant <- unique(dataset$Segment.Type)
   return (ret)
 } 
+
