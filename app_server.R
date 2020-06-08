949cf3aa78eb6791184bd0a0a2ae4cc794298219
christiancruz-124 <cruzc124@uw.edu>
2020-06-07 17:17:00 -0700

    add angle to x axis

--- app_server.R
+++ app_server.R
@@ -103,7 +103,7 @@ server <- function(input, output) {
       geom_col(position = "fill") +
       scale_fill_brewer(name = "Opinion", palette = "Set1") +
       xlab(input$select) +
-      ylab("Count")
+      ylab("Count") +
       theme(
         axis.text.x = element_text(angle = 45, hjust = 1)
       )
